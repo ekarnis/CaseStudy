c4f16e5bb1519d551087660d389ce363baa5b000
Eric Karnis <ekarnis@outlook.com>
2018-05-14 12:04:52 -0700

    split the sp file so each class gets its own sp file

--- SQL/user_SP.sql
+++ SQL/user_SP.sql
@@ -1,18 +1,3 @@
-create or replace procedure sp_insert_user_status(user_status_id number, user_status varchar)
-as 
-begin
-  insert into user_statuses
-  values(user_status_id, user_status);
-  
-end;
-
-create or replace procedure sp_update_user_status(user_status_num number, user_status_name varchar)
-as 
-begin
-  update user_statuses
-  set user_status=user_status_name
-  where user_status_id=user_status_num;
-end;
 
 create or replace procedure sp_update_user(user_num number, firstN varchar, lastN varchar, eml varchar, psswd varchar, u_s_id number, loc_id number)
 as 
@@ -30,16 +15,9 @@ begin
   
 end;
 
-create or replace procedure sp_delete_user_status_by_id(user_status_num number)
-as 
-begin
-  Delete from user_statuses where user_status_id = user_status_num;
-end;
-
 create or replace procedure sp_delete_user_by_id(user_num number)
 as 
 begin
   Delete from users where user_id = user_num;
 end;
 
-show errors;
