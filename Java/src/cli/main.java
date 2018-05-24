80b1a68cc52def064dc37bc58a1dccd304351d8a
Eric Karnis <ekarnis@outlook.com>
2018-05-23 17:09:45 -0700

    small changes to main.java

--- Java/src/cli/main.java
+++ Java/src/cli/main.java
@@ -8,21 +8,46 @@ public class main {
 	public static void main(String[] args) {
 
 		System.out.println("q: quit	b:go back #:select option");
-		
+
+		initialScreen();
+	}
+	
+	public static void initialScreen(){
 		ArrayList<String> options = new ArrayList<String>();
 		options.add("Login");
 		options.add("Register");
 		options.add("Menu");
 		printOptions(options);
-		
 		Scanner sc = new Scanner(System.in);
-	    int input = sc.nextInt();
-	    
-	    
-	    
-	
-		
+	    String input = sc.next();
+	    switch(input){
+    		case "1":
+    			loginScreen();
+    		case "2":
+    			registerScreen();    	
+    		case "3":
+    			menuScreen();
+    		case "q":
+    			System.exit(0);
+	    }
 	    sc.close();
+
+	}
+	public static void loginScreen(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("q: quit	b:go back #:select option");
+	    String input = sc.next();
+
+	    
+	    
+	    sc.close();
+
+	}
+	public static void registerScreen(){
+		
+	}
+	public static void menuScreen(){
+		
 	}
 	
 	public static void printOptions(ArrayList<String> options){
