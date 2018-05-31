6ba565356f44341b0a4a8b5b7d2e2e43d7e71ae6
fmb3codes <fmb34@nau.edu>
2018-05-31 14:28:38 -0700

    Made page/css modifications and started login controller

--- spring/src/main/java/com/tigers/controllers/IndexController.java
+++ spring/src/main/java/com/tigers/controllers/IndexController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FrontController {
+public class IndexController { 
 	
 	@RequestMapping("/")
 	public String index() {
-		return "index";
+		return "home";
 	}
 }
