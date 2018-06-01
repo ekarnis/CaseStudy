85b81c97b1e93a19fc4fe812575674c0407c4618
Jordan Goodman <gojordang@gmail.com>
2018-06-01 14:09:05 -0700

    Added JDBC dependency

--- Java/src/cli/Tiger.java
+++ Java/src/cli/Tiger.java
@@ -28,7 +28,7 @@ public class Tiger{
 	public static void main(String[] args) {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-	        con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "pass", "pass");
+	        con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
