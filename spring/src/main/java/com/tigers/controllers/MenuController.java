9768266a47127df941bd72f10379839f5deaa3e6
Jordan Goodman <gojordang@gmail.com>
2018-06-07 11:50:52 -0700

    Menu Items now displaying

--- spring/src/main/java/com/tigers/controllers/MenuController.java
+++ spring/src/main/java/com/tigers/controllers/MenuController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tigers.services.UserService;
@@ -24,6 +25,20 @@ public class MenuController {
 		List<Menu> items = menServ.list();
 		System.out.println(items.toString());
 		return "menu";
+	}
+	
+	@ModelAttribute("items")
+	public String menuItemsTable(){
+		String itemTable = "<table> <tr><th>Item</th><th>Picture</th><th>Description</th><th>Price</th><th>Add</th></tr>";
+		List<Menu> items = menServ.list();
+		for(Menu item:items) {
+			itemTable += "<tr><td>" + item.getName() + "</td><td><img src=\"" + item.getPhoto() + "\" width=\"150\"</td><td>" + 
+						item.getDescription() + "</td><td>$" + item.getPrice() + "</td><td><button>Add</button></td></tr>";
+		}
+		
+		itemTable += "</table>";
+		
+		return itemTable;
 	}
 	
 }
