b2b4762c41cbb80fbcc77efdedeeb7eb96615228
DManuel010 <d.manuel010@gmail.com>
2018-06-07 14:03:19 -0700

    delete some imports

--- spring/src/main/java/com/tigers/controllers/OrderController.java
+++ spring/src/main/java/com/tigers/controllers/OrderController.java
@@ -1,11 +1,9 @@
 package com.tigers.controllers;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
